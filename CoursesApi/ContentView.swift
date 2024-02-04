17bce51452a1ccc802e725713c3816c1153dcd62
joaobug <joaoenfrance@gmail.com>
2024-02-04 17:09:49 -0300

    Initial Commit

---
+++ CoursesApi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoursesApi
+//
+//  Created by Joao Leal on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoursesApi/CoursesApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApiApp.swift
+//  CoursesApi
+//
+//  Created by Joao Leal on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
